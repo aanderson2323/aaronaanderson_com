7804d0b2b05fe7f8f422c75f215eeaf74f26e3c4
aaron <aanderson2323@gmail.com>
2020-04-12 14:17:49 -0500

    ignore dist, update nav

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,6 +6,5 @@ export default () => <div id="contact">
     <ul>
         <li><div>Email:</div> <div><a href="mailto:me@aaronaanderson.com">me@aaronaanderson.com</a></div></li>
         <li><div>LinkedIn: </div><div><a href="https://www.linkedin.com/in/aaron-anderson-21230199/">https://www.linkedin.com/in/aaron-anderson-21230199/</a></div></li>
-        <li><div>Twitter:</div><div> <a href="https://twitter.com/_Aaron_A_">https://twitter.com/_Aaron_A_</a></div></li>
     </ul>
 </div>;
--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -4,9 +4,6 @@ export const NavBar = () => {
     return <>
         <ul id="nav">
             <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
                 <Link to="/about">About</Link>
             </li>
             <li>
