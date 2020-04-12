b450f7586d3ec569ca5d88b0b706ceb255955ce4
aaron <aanderson2323@gmail.com>
2020-04-11 20:07:35 -0500

    move nav bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { NavBar } from './components/navBar'
 import Hero from './components/Hero';
 import Sections from './components/Sections';
 
 export default () =>
   <div className="App">
-    <Hero />
     <Router>
-      <NavBar />
+      <Hero />
       <Sections />
     </Router >
   </div>;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { NavBar } from './navBar'
+
 export default () => <div className="col-md-9 col-lg-10 col-xl-11 text-center">
-    <div id="home" className="flex-container">
+    <div id="hero" className="flex-container">
         <div className="jumbotron">
             <h1 className="display-4">Hi, I'm Aaron!</h1>
             <hr className="my-4" />
@@ -8,5 +10,6 @@ export default () => <div className="col-md-9 col-lg-10 col-xl-11 text-center">
             <p className="lead">What do you do Aaron?</p>
             <p>I read. I write. I code.</p>
         </div>
+        <NavBar />
     </div>
 </div>;
